3c70ad8d6f2fa186ec8a9b89791eaba242f57ac7
Aldrin <aldrinmalakar@gmail.com>
2020-09-17 01:42:25 -0400

    Aye pappi

--- src/Basic/demoGitHubUpload.java
+++ src/Basic/demoGitHubUpload.java
@@ -3,6 +3,18 @@ package Basic;
 public class demoGitHubUpload {
     public static void main(String[] args) {
         System.out.println("Hello World on GitHub");
+        for ( int i = 1; i< 5; i++){
+            System.out.print("*?");
+            for (int a = 1; a<6-i; a++) {
+                System.out.print("@");
+                for (int b = 1; b < 5-a;b++){
+                    System.out.print("*");
+                    for (int c=1; c<5+1; c++);
+                }
+                System.out.println(" ");
+            }
+        }
     }
+
 
 }
